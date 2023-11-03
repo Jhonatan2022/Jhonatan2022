bff5f63816265011b3d586535998feac0029680d
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-11-02 20:01:50 -0500

    Update Config

---
+++ .vscode-config/configVscode.ts
@@ -0,0 +1,66 @@
+const CONFIG_VSCODE = {
+  'editor.inlineSuggest.enabled': true,
+  'files.autoSave': 'afterDelay',
+  'editor.scrollbar.vertical': 'hidden',
+  'editor.overviewRulerBorder': false,
+  'workbench.iconTheme': 'material-icon-theme',
+  'editor.guides.indentation': false,
+  'indenticator.width': 0.1,
+  'indenticator.color.dark': 'rgba(255, 255, 255, 0.20)',
+  'editor.wordWrap': 'on',
+  'github.copilot.enable': {
+    '*': true,
+    plaintext: false,
+    markdown: true,
+    scminput: false,
+    yaml: false
+  },
+  '[javascript]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode'
+  },
+  'editor.matchBrackets': 'never',
+  'pasteImage.defaultName': 'name',
+  'pasteImage.showFilePathConfirmInputBox': true,
+
+  'git.autofetch': true,
+  'security.workspace.trust.untrustedFiles': 'open',
+  'terminal.integrated.env.windows': {},
+  'terminal.integrated.tabs.location': 'left',
+  'terminal.integrated.tabs.enabled': false,
+  '[css]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode'
+  },
+  'explorer.confirmDragAndDrop': false,
+  'liveServer.settings.donotVerifyTags': true,
+  'liveServer.settings.donotShowInfoMsg': true,
+  'githubPullRequests.pullBranch': 'never',
+  'window.zoomLevel': 1.1,
+  'terminal.integrated.env.linux': {},
+  'wakatime.apiKey': '',
+  launch: {
+    configurations: [],
+    compounds: []
+  },
+  'editor.codeActionsOnSave': {
+    'source.fixAll.eslint': true,
+    'source.formatDocument': true
+  },
+  '[json]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode'
+  },
+  '[javascriptreact]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode'
+  },
+  '[typescriptreact]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode'
+  },
+  'prettier.semi': false,
+  'prettier.singleQuote': true,
+  'prettier.trailingComma': 'none',
+  'typescript.format.semicolons': 'ignore',
+  'typescript.format.insertSpaceBeforeFunctionParenthesis': true,
+  '[html]': {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode'
+  },
+  'workbench.colorTheme': "SynthWave '84"
+}
